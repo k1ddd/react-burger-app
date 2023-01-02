593d5f9099658460de8873bdcf94aada42941af4
vladyslav <vladyslav.klokun@gmail.com>
2023-01-02 20:40:20 +0200

    finish hW24

--- burger/src/components/Forma/Forma.js
+++ burger/src/components/Forma/Forma.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { Formik, Field, Form } from "formik";
 import "../../index.css";
+import onion from "../../assets/onion.png";
+import cheese from "../../assets/cheese.png";
+import tomato from "../../assets/tomato.png";
+import lettuce from "../../assets/lettuce.png";
+import meat from "../../assets/meat.png";
+import pickles from "../../assets/pickles.png";
 
 export default function Forma({ modalIngr, burgerPrice, setModal }) {
   const initialValues = {
@@ -48,42 +54,73 @@ export default function Forma({ modalIngr, burgerPrice, setModal }) {
     <div className="forma">
       <h1>Order</h1>
 
-      <ul>
-        {modalIngr.onion > 0 && <li>onion: {modalIngr.onion}</li>}
-        {modalIngr.tomato > 0 && <li>tomato: {modalIngr.tomato}</li>}
-        {modalIngr.cheese > 0 && <li>cheese: {modalIngr.cheese}</li>}
-        {modalIngr.salad > 0 && <li>salad: {modalIngr.salad}</li>}
-        {modalIngr.meat > 0 && <li>meat: {modalIngr.meat}</li>}
-        {modalIngr.pickles > 0 && <li>pickles: {modalIngr.pickles}</li>}
+      <ul className="order__list">
+        {modalIngr.onion > 0 && (
+          <li>
+            <img src={onion} className="ingredients" alt="Logo" />
+            {modalIngr.onion}
+          </li>
+        )}
+        {modalIngr.tomato > 0 && (
+          <li>
+            <img src={tomato} className="ingredients" alt="Logo" />
+            {modalIngr.tomato}
+          </li>
+        )}
+        {modalIngr.cheese > 0 && (
+          <li>
+            <img src={cheese} className="ingredients" alt="Logo" />
+            {modalIngr.cheese}
+          </li>
+        )}
+        {modalIngr.salad > 0 && (
+          <li>
+            <img src={lettuce} className="ingredients" alt="Logo" />
+            {modalIngr.salad}
+          </li>
+        )}
+        {modalIngr.meat > 0 && (
+          <li>
+            <img src={meat} className="ingredients" alt="Logo" />
+            {modalIngr.meat}
+          </li>
+        )}
+        {modalIngr.pickles > 0 && (
+          <li>
+            <img src={pickles} className="ingredients" alt="Logo" />
+            {modalIngr.pickles}
+          </li>
+        )}
       </ul>
 
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         <Form className="fields">
           <Field name="name" placeholder="Your name" />
-
           <Field name="mail" placeholder="Your mail" />
-
           <Field name="phone" placeholder="Your phone" />
-
           <Field name="address" placeholder="Your address" />
 
-          <div>
+          <div className="order__btn">
             <button
-              className="111"
+              className="order__and__cancel"
               type="button"
               onClick={() => setModal(false)}
             >
               cancel
             </button>
-            <button type="submit" onSubmit={handleSubmit}>
+            <button
+              className="order__and__cancel"
+              type="submit"
+              onSubmit={handleSubmit}
+            >
               order
             </button>
           </div>
         </Form>
       </Formik>
       <div className="totalPrice">
-        total price:
-        <h2>{burgerPrice}</h2>
+        Total price:
+        <h2 className="orderBurgerPrice">{burgerPrice} UAH</h2>
       </div>
     </div>
   );
